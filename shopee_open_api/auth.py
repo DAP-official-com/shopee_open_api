ac2699d7ff445708a9d5450593abffb8e19f3e9a
Siwat Tansiri <tansirijames@gmail.com>
2021-10-12 17:06:13 +0700

    Add new branch upon authorization and remove branch upon unauthorization

--- shopee_open_api/auth.py
+++ shopee_open_api/auth.py
@@ -1,18 +1,21 @@
 import frappe
-import json
-import hmac, time, requests, hashlib
+from datetime import datetime
 from .python_shopee import pyshopee2
-
 
 PARTNER_ID = frappe.db.get_single_value("Shopee API Settings", "partner_id")
 PARTNER_KEY = frappe.db.get_single_value("Shopee API Settings", "partner_key")
-REDIRECT_URL = "http://localhost:8000/api/method/shopee_open_api.auth.authenticate_after_authorization"
+AUTHORIZE_REDIRECT_URL = (
+    "http://localhost:8000/api/method/shopee_open_api.auth.authorize_callback"
+)
+UNAUTHORIZE_REDIRECT_URL = (
+    "http://localhost:8000/api/method/shopee_open_api.auth.unauthorize_callback"
+)
 
 client = pyshopee2.Client(
     shop_id=0,
     partner_id=PARTNER_ID,
     partner_key=PARTNER_KEY,
-    redirect_url=REDIRECT_URL,
+    redirect_url=AUTHORIZE_REDIRECT_URL,
     test_env=True,
 )
 
@@ -20,59 +23,84 @@ client = pyshopee2.Client(
 @frappe.whitelist()
 def authorize():
 
-    url = client.shop_authorization(redirect_url=REDIRECT_URL)
+    url = client.shop_authorization(redirect_url=AUTHORIZE_REDIRECT_URL)
 
     frappe.local.response["type"] = "redirect"
     frappe.local.response["location"] = url
 
 
 @frappe.whitelist()
-def authenticate_after_authorization():
-
-    timest = int(time.time())
-    host = "https://partner.test-stable.shopeemobile.com"
-    path = "/api/v2/auth/token/get"
+def authorize_callback():
 
     code = frappe.request.args.getlist("code")[0]
     shop_id = int(frappe.request.args.getlist("shop_id")[0])
 
-    base_string = "%s%s%s" % (PARTNER_ID, path, timest)
+    client.code = code
+    client.shop_id = shop_id
+    client.get_token()
 
-    sign = hmac.new(
-        PARTNER_KEY.encode("utf-8"), base_string.encode("utf-8"), hashlib.sha256
-    ).hexdigest()
+    shop_profile = client.shop.get_profile()["response"]
+    shop_profile.update(client.shop.get_shop_info())
 
-    body = {
-        "code": code,
-        "shop_id": shop_id,
-        "partner_id": PARTNER_ID,
-    }
-
-    url = (
-        host + path + "?partner_id=%s&timestamp=%s&sign=%s" % (PARTNER_ID, timest, sign)
+    shopee_branch_exists = frappe.db.exists(
+        {"doctype": "Branch", "shopee_shop_id": shop_id}
     )
 
-    headers = {"Content-Type": "application/json"}
-    r = requests.post(url, json=body, headers=headers)
+    if not shopee_branch_exists:
 
-    content = r.json()
-    access_token = content["access_token"]
+        doc = frappe.new_doc("Branch")
+        doc.branch = shop_profile["shop_name"]
+        doc.shopee_shop_id = shop_id
+        doc.shopee_shop_logo = shop_profile["shop_logo"]
+        doc.shopee_shop_status = shop_profile["status"]
+        doc.shopee_shop_description = shop_profile["description"]
 
-    path = "/api/v2/shop/get_shop_info"
-    base_string = "%s%s%s%s%s" % (PARTNER_ID, path, timest, access_token, shop_id)
+        doc.shopee_shop_authorize_time = datetime.utcfromtimestamp(
+            shop_profile["auth_time"],
+        ).strftime("%Y-%m-%d %H:%M:%S")
 
-    sign = hmac.new(
-        PARTNER_KEY.encode("utf-8"), base_string.encode("utf-8"), hashlib.sha256
-    ).hexdigest()
+        doc.shopee_shop_expire_time = datetime.utcfromtimestamp(
+            shop_profile["expire_time"],
+        ).strftime("%Y-%m-%d %H:%M:%S")
 
-    url = (
-        host
-        + path
-        + "?partner_id=%s&timestamp=%s&access_token=%s&shop_id=%s&sign=%s"
-        % (PARTNER_ID, timest, access_token, shop_id, sign)
+        doc.insert()
+        frappe.db.commit()
+
+    return {"message": "ok"}
+
+
+@frappe.whitelist()
+def unauthorize():
+
+    sign, unauth_url = client.auth_url("/api/v2/shop/cancel_auth_partner")
+    unauth_url += f"&redirect={UNAUTHORIZE_REDIRECT_URL}"
+
+    frappe.local.response["type"] = "redirect"
+    frappe.local.response["location"] = unauth_url
+
+
+@frappe.whitelist()
+def unauthorize_callback():
+
+    shop_id = int(frappe.request.args.getlist("shop_id")[0])
+
+    shop_exists = frappe.db.exists(
+        {
+            "doctype": "Branch",
+            "shopee_shop_id": shop_id,
+        }
     )
 
-    r = requests.get(url)
-    print(r.json())
+    if shop_exists:
 
-    return content
+        branch_name = shop_exists[0][0]
+
+        shop = frappe.get_doc(
+            "Branch",
+            branch_name,
+        )
+
+        shop.delete()
+        frappe.db.commit()
+
+    return {"message": "ok"}
