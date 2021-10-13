cf2ce4caa9168fd270dbdb43ff8df67f983cab50
Siwat Tansiri <tansirijames@gmail.com>
2021-10-13 09:22:20 +0700

    Add 'Add shopeeshop' button to branch list view

---
+++ shopee_open_api/public/js/branch_list.js
@@ -0,0 +1,23 @@
+frappe.listview_settings["Branch"] = {
+
+	onload: function(listview) {
+		this.add_button(["Add shopee as a new branch"], "default", function() { 
+			// can I add something here?
+			frappe.call({
+				method: "shopee_open_api.auth.get_authorize_url",
+				callback: function(r) {
+					let authorizeUrl = r['message'];
+					window.open(authorizeUrl, "_blank");
+				}
+			})
+		})
+	},
+
+	add_button(name, type, action, wrapper_class=".page-actions") {
+		const button = document.createElement("button");
+		button.classList.add("btn", "btn-" + type, "btn-sm", "ml-2");
+		button.innerHTML = name;
+		button.onclick = action;
+		document.querySelector(wrapper_class).prepend(button);
+	},
+};
