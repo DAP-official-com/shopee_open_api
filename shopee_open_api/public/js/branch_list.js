ae51e4b4d98d2dcd468890e9313aae872740e21c
Siwat Tansiri <tansirijames@gmail.com>
2021-10-13 10:50:18 +0700

    Minor formatting

--- shopee_open_api/public/js/branch_list.js
+++ shopee_open_api/public/js/branch_list.js
@@ -1,21 +1,20 @@
-frappe.listview_settings["Branch"] = {
-
-	onload: function(listview) {
-		this.add_button(["Add shopee as a new branch"], "default", function() { 
+frappe.listview_settings['Branch'] = {
+	onload: function (listview) {
+		this.add_button(['Add shopee as a new branch'], 'default', function () {
 			// can I add something here?
 			frappe.call({
-				method: "shopee_open_api.auth.get_authorize_url",
-				callback: function(r) {
+				method: 'shopee_open_api.auth.get_authorize_url',
+				callback: function (r) {
 					let authorizeUrl = r['message'];
-					window.open(authorizeUrl, "_self");
-				}
-			})
-		})
+					window.open(authorizeUrl, '_self');
+				},
+			});
+		});
 	},
 
-	add_button(name, type, action, wrapper_class=".page-actions") {
-		const button = document.createElement("button");
-		button.classList.add("btn", "btn-" + type, "btn-sm", "ml-2");
+	add_button(name, type, action, wrapper_class = '.page-actions') {
+		const button = document.createElement('button');
+		button.classList.add('btn', 'btn-' + type, 'btn-sm', 'ml-2');
 		button.innerHTML = name;
 		button.onclick = action;
 		document.querySelector(wrapper_class).prepend(button);
