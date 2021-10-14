6e85236c0e938af5eeeb580b11ff887dee467772
Siwat Tansiri <tansirijames@gmail.com>
2021-10-14 15:13:16 +0700

    Remove token refreshing

--- shopee_open_api/apis/shop.py
+++ shopee_open_api/apis/shop.py
@@ -9,14 +9,6 @@ def reload_shop_details_from_shopee(branch_name):
     branch = frappe.get_doc("Branch", branch_name)
     client = get_client_from_branch(branch)
 
-    if client.is_token_expired:
-
-        client.refresh_current_token()
-
-        branch.shopee_access_token = client.access_token
-        branch.shopee_refresh_token = client.refresh_token
-        branch.shopee_token_expiration_unix = int(time.time()) + client.timeout
-
     shop_profile = client.shop.get_profile()["response"]
     shop_profile.update(client.shop.get_shop_info())
 
