04b0e2af0ea8a294b546485f33d64f5a827998c3
Siwat Tansiri <tansirijames@gmail.com>
2021-10-27 21:46:45 +0700

    Add refresh order button

---
+++ shopee_open_api/apis/order.py
@@ -0,0 +1,30 @@
+import frappe
+from shopee_open_api.exceptions import BadRequestError
+from shopee_open_api.utils.client import get_client_from_shop_id
+from shopee_open_api.shopee_models.order import Order
+
+
+@frappe.whitelist()
+def reload_order_details_from_shopee(shop_id, order_sn):
+
+    client = get_client_from_shop_id(shop_id)
+
+    order_detail_response = client.order.get_order_detail(
+        order_sn_list=order_sn,
+        response_optional_fields="buyer_user_id,buyer_username,estimated_shipping_fee,recipient_address,actual_shipping_fee,goods_to_declare,note,note_update_time,item_list,pay_time,dropshipper,credit_card_number,dropshipper_phone,split_up,buyer_cancel_reason,cancel_by,cancel_reason,actual_shipping_fee_confirmed,buyer_cpf_id,fulfillment_flag,pickup_done_time,package_list,shipping_carrier,payment_method,total_amount,buyer_username,invoice_data,checkout_shipping_carrier,reverse_shipping_fee",
+    )
+
+    if order_detail_response.get("error"):
+        raise BadRequestError(
+            f"{order_detail_response.get('error')} {order_detail_response.get('message')}"
+        )
+
+    order_details = order_detail_response["response"]["order_list"][0]
+
+    order = Order(order_details, shop_id=shop_id)
+
+    order.update_or_insert_with_items()
+
+    frappe.db.commit()
+
+    return {"message": "ok"}
--- shopee_open_api/shopee_open_api/doctype/shopee_order/shopee_order.js
+++ shopee_open_api/shopee_open_api/doctype/shopee_order/shopee_order.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Shopee Order', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function (frm) {
+		frm.add_custom_button('Refresh from Shopee', () => {
+			frappe.call({
+				method: 'shopee_open_api.apis.order.reload_order_details_from_shopee',
+				args: {
+					shop_id: frm.doc.shopee_shop,
+					order_sn: frm.doc.order_sn,
+				},
+				callback: function (r) {
+					frm.refresh();
+				},
+			});
+		});
+	},
 });
