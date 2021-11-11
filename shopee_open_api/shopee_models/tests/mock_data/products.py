79e78401f63f94b878f229d17e505e3817a78af7
Siwat Tansiri <tansirijames@gmail.com>
2021-11-11 17:40:54 +0700

    Add order update_or_insert_with_items test

--- shopee_open_api/shopee_models/tests/mock_data/products.py
+++ shopee_open_api/shopee_models/tests/mock_data/products.py
@@ -10450,86 +10450,320 @@ product_base_info = [
     },
 ]
 
-get_model_list_response = {
-    'response' :{
-    "tier_variation": [
-        {
-            "name": "Characteristic",
-            "option_list": [
-                {
-                    "option": "Deceptive",
-                    "image": {
-                        "image_id": "d82a222d763a63be6aa726236c2a65e1",
-                        "image_url": "https://cf.shopee.co.th/file/d82a222d763a63be6aa726236c2a65e1",
+get_item_base_info_response = {
+    "error": "",
+    "message": "",
+    "warning": "",
+    "request_id": "1a269bebb13e1ac4b38097c2d7698b48",
+    "response": {
+        "item_list": [
+            {
+                "item_id": 101567911,
+                "category_id": 100143,
+                "item_name": "Hottest item rn 98",
+                "description": "This is just a nice description for a product",
+                "item_sku": "",
+                "create_time": 1635502633,
+                "update_time": 1636030271,
+                "attribute_list": [
+                    {
+                        "attribute_id": 100963,
+                        "original_attribute_name": "FDA Registration No.",
+                        "is_mandatory": True,
+                        "attribute_value_list": [
+                            {
+                                "value_id": 0,
+                                "original_value_name": "FDA Regis No. 0000001",
+                                "value_unit": "",
+                            }
+                        ],
+                    }
+                ],
+                "price_info": [
+                    {"currency": "THB", "original_price": 1500, "current_price": 1500}
+                ],
+                "stock_info": [
+                    {
+                        "stock_type": 2,
+                        "current_stock": 300,
+                        "normal_stock": 300,
+                        "reserved_stock": 0,
+                    }
+                ],
+                "image": {
+                    "image_url_list": [
+                        "https://cf.shopee.co.th/file/b56b2e483555164c8f4c05ff915d175d"
+                    ],
+                    "image_id_list": ["b56b2e483555164c8f4c05ff915d175d"],
+                },
+                "weight": "1.000",
+                "dimension": {
+                    "package_length": 0,
+                    "package_width": 0,
+                    "package_height": 0,
+                },
+                "logistic_info": [
+                    {
+                        "logistic_id": 7032,
+                        "logistic_name": "Standard Delivery - ส่งธรรมดาในประเทศ",
+                        "enabled": True,
+                        "is_free": False,
+                        "estimated_shipping_fee": 0,
                     },
-                },
-                {
-                    "option": "More Deceptive",
-                    "image": {
-                        "image_id": "b6ed2b13f5d58438c69f8922af34a06a",
-                        "image_url": "https://cf.shopee.co.th/file/b6ed2b13f5d58438c69f8922af34a06a",
+                    {
+                        "logistic_id": 70021,
+                        "logistic_name": "Kerry",
+                        "enabled": False,
+                        "shipping_fee": 100,
+                        "is_free": False,
                     },
+                    {
+                        "logistic_id": 70022,
+                        "logistic_name": "J&T Express",
+                        "enabled": False,
+                        "shipping_fee": 0,
+                        "is_free": False,
+                    },
+                    {
+                        "logistic_id": 78009,
+                        "logistic_name": "Ninja Van",
+                        "enabled": False,
+                        "shipping_fee": 0,
+                        "is_free": False,
+                    },
+                    {
+                        "logistic_id": 70066,
+                        "logistic_name": "Shopee Express",
+                        "enabled": False,
+                        "shipping_fee": 0,
+                        "is_free": False,
+                    },
+                ],
+                "pre_order": {"is_pre_order": False, "days_to_ship": 2},
+                "condition": "NEW",
+                "size_chart": "",
+                "item_status": "NORMAL",
+                "has_model": True,
+                "promotion_id": 0,
+                "brand": {
+                    "brand_id": 1116993,
+                    "original_brand_name": "FREELOX(ฟรีล็อก)",
                 },
-            ],
-        }
-    ],
-    "model": [
-        {
-            "model_id": 9379156,
-            "promotion_id": 0,
-            "tier_index": [0],
-            "stock_info": [
-                {
-                    "stock_type": 2,
-                    "current_stock": 78,
-                    "normal_stock": 78,
-                    "reserved_stock": 0,
-                },
-                {
-                    "stock_type": 1,
-                    "current_stock": 0,
-                    "normal_stock": 0,
-                    "reserved_stock": 0,
-                },
-            ],
-            "price_info": [
-                {
-                    "current_price": 1000,
-                    "original_price": 1000,
-                    "inflated_price_of_current_price": 1000,
-                    "inflated_price_of_original_price": 1000,
-                }
-            ],
-            "pre_order": {"is_pre_order": False, "days_to_ship": 7},
-        },
-        {
-            "model_id": 9379157,
-            "promotion_id": 0,
-            "tier_index": [1],
-            "stock_info": [
-                {
-                    "stock_type": 2,
-                    "current_stock": 148,
-                    "normal_stock": 148,
-                    "reserved_stock": 0,
-                },
-                {
-                    "stock_type": 1,
-                    "current_stock": 0,
-                    "normal_stock": 0,
-                    "reserved_stock": 0,
-                },
-            ],
-            "price_info": [
-                {
-                    "current_price": 2000,
-                    "original_price": 2000,
-                    "inflated_price_of_current_price": 2000,
-                    "inflated_price_of_original_price": 2000,
-                }
-            ],
-            "pre_order": {"is_pre_order": False, "days_to_ship": 7},
-        },
-    ],
+                "item_dangerous": 0,
+            }
+        ]
+    },
 }
+
+
+get_model_list_response = {
+    "error": "",
+    "message": "",
+    "warning": "",
+    "request_id": "86446739444c34bef3b10d794e6a70f4",
+    "response": {
+        "tier_variation": [
+            {
+                "name": "Color",
+                "option_list": [
+                    {
+                        "option": "Black",
+                        "image": {
+                            "image_id": "61cd08e4112e75090e09ec07fdc31fc8",
+                            "image_url": "https://cf.shopee.co.th/file/61cd08e4112e75090e09ec07fdc31fc8",
+                        },
+                    },
+                    {
+                        "option": "White",
+                        "image": {
+                            "image_id": "5250d4da05f015cc630323c5f0dd5770",
+                            "image_url": "https://cf.shopee.co.th/file/5250d4da05f015cc630323c5f0dd5770",
+                        },
+                    },
+                ],
+            },
+            {
+                "name": "Size",
+                "option_list": [
+                    {"option": "Fat"},
+                    {"option": "Not Fat"},
+                    {"option": "Super,Fat"},
+                ],
+            },
+        ],
+        "model": [
+            {
+                "model_id": 20009246845,
+                "promotion_id": 0,
+                "tier_index": [1, 0],
+                "stock_info": [
+                    {
+                        "stock_type": 2,
+                        "current_stock": 19,
+                        "normal_stock": 19,
+                        "reserved_stock": 0,
+                    },
+                    {
+                        "stock_type": 1,
+                        "current_stock": 0,
+                        "normal_stock": 0,
+                        "reserved_stock": 0,
+                    },
+                ],
+                "price_info": [
+                    {
+                        "current_price": 50,
+                        "original_price": 50,
+                        "inflated_price_of_current_price": 50,
+                        "inflated_price_of_original_price": 50,
+                    }
+                ],
+                "model_sku": "wf",
+                "pre_order": {"is_pre_order": False, "days_to_ship": 2},
+            },
+            {
+                "model_id": 20009246846,
+                "promotion_id": 0,
+                "tier_index": [1, 1],
+                "stock_info": [
+                    {
+                        "stock_type": 2,
+                        "current_stock": 20,
+                        "normal_stock": 20,
+                        "reserved_stock": 0,
+                    },
+                    {
+                        "stock_type": 1,
+                        "current_stock": 0,
+                        "normal_stock": 0,
+                        "reserved_stock": 0,
+                    },
+                ],
+                "price_info": [
+                    {
+                        "current_price": 60,
+                        "original_price": 60,
+                        "inflated_price_of_current_price": 60,
+                        "inflated_price_of_original_price": 60,
+                    }
+                ],
+                "model_sku": "wn",
+                "pre_order": {"is_pre_order": False, "days_to_ship": 2},
+            },
+            {
+                "model_id": 20009246847,
+                "promotion_id": 0,
+                "tier_index": [0, 0],
+                "stock_info": [
+                    {
+                        "stock_type": 2,
+                        "current_stock": 17,
+                        "normal_stock": 17,
+                        "reserved_stock": 0,
+                    },
+                    {
+                        "stock_type": 1,
+                        "current_stock": 0,
+                        "normal_stock": 0,
+                        "reserved_stock": 0,
+                    },
+                ],
+                "price_info": [
+                    {
+                        "current_price": 50,
+                        "original_price": 50,
+                        "inflated_price_of_current_price": 50,
+                        "inflated_price_of_original_price": 50,
+                    }
+                ],
+                "model_sku": "bf",
+                "pre_order": {"is_pre_order": False, "days_to_ship": 2},
+            },
+            {
+                "model_id": 20009246848,
+                "promotion_id": 0,
+                "tier_index": [0, 1],
+                "stock_info": [
+                    {
+                        "stock_type": 2,
+                        "current_stock": 18,
+                        "normal_stock": 18,
+                        "reserved_stock": 0,
+                    },
+                    {
+                        "stock_type": 1,
+                        "current_stock": 0,
+                        "normal_stock": 0,
+                        "reserved_stock": 0,
+                    },
+                ],
+                "price_info": [
+                    {
+                        "current_price": 60,
+                        "original_price": 60,
+                        "inflated_price_of_current_price": 60,
+                        "inflated_price_of_original_price": 60,
+                    }
+                ],
+                "model_sku": "bn",
+                "pre_order": {"is_pre_order": False, "days_to_ship": 2},
+            },
+            {
+                "model_id": 20009248886,
+                "promotion_id": 0,
+                "tier_index": [0, 2],
+                "stock_info": [
+                    {
+                        "stock_type": 2,
+                        "current_stock": 198,
+                        "normal_stock": 198,
+                        "reserved_stock": 0,
+                    },
+                    {
+                        "stock_type": 1,
+                        "current_stock": 0,
+                        "normal_stock": 0,
+                        "reserved_stock": 0,
+                    },
+                ],
+                "price_info": [
+                    {
+                        "current_price": 50,
+                        "original_price": 50,
+                        "inflated_price_of_current_price": 50,
+                        "inflated_price_of_original_price": 50,
+                    }
+                ],
+                "pre_order": {"is_pre_order": False, "days_to_ship": 2},
+            },
+            {
+                "model_id": 20009248887,
+                "promotion_id": 0,
+                "tier_index": [1, 2],
+                "stock_info": [
+                    {
+                        "stock_type": 2,
+                        "current_stock": 98,
+                        "normal_stock": 98,
+                        "reserved_stock": 0,
+                    },
+                    {
+                        "stock_type": 1,
+                        "current_stock": 0,
+                        "normal_stock": 0,
+                        "reserved_stock": 0,
+                    },
+                ],
+                "price_info": [
+                    {
+                        "current_price": 800,
+                        "original_price": 800,
+                        "inflated_price_of_current_price": 800,
+                        "inflated_price_of_original_price": 800,
+                    }
+                ],
+                "pre_order": {"is_pre_order": False, "days_to_ship": 2},
+            },
+        ],
+    },
 }
--- shopee_open_api/shopee_models/tests/test_order.py
+++ shopee_open_api/shopee_models/tests/test_order.py
@@ -4,7 +4,7 @@ from shopee_open_api.utils.client import get_shopless_client
 from shopee_open_api.utils.datetime import datetime_string_from_unix
 from .mock_data.orders import get_order_detail_response
 from .mock_data.payments import get_escrow_detail_response
-from .mock_data.products import product_base_info
+from .mock_data.products import get_item_base_info_response, get_model_list_response
 from shopee_open_api.shopee_models.order import Order
 from shopee_open_api.shopee_models.payment_escrow import PaymentEscrow
 from shopee_open_api.shopee_models.order_item import OrderItem
@@ -47,23 +47,23 @@ class ShopeeProductTest(TestCase):
         shop.insert()
         cls.shop = shop
 
+        cls.ORDER_ID = uuid.uuid4().hex.upper()[0:6]
+        order_detail_response = get_order_detail_response.copy()
+        order_detail_response["response"]["order_list"][0]["order_sn"] = cls.ORDER_ID
+
         cls.order = Order(
-            get_order_detail_response["response"]["order_list"][0], shop_id=cls.SHOP_ID
+            order_detail_response["response"]["order_list"][0],
+            shop_id=cls.SHOP_ID,
         )
+
+        cls.fake_item_id = uuid.uuid4().hex.upper()[:6]
+
+        for order_item in cls.order.order_items:
+            order_item.item_id = cls.fake_item_id
 
     @classmethod
     def tearDownClass(cls):
         cls.patcher.stop()
-
-        product_ids = frappe.db.get_list(
-            "Shopee Product",
-            filters={
-                "shopee_shop": cls.SHOP_ID,
-            },
-            pluck="name",
-        )
-        for product_id in product_ids:
-            frappe.get_doc("Shopee Product", product_id).delete()
 
         order_ids = frappe.db.get_list(
             "Shopee Order",
@@ -75,11 +75,32 @@ class ShopeeProductTest(TestCase):
         for order_id in order_ids:
             frappe.get_doc("Shopee Order", order_id).delete()
 
+        product_ids = frappe.db.get_list(
+            "Shopee Product",
+            filters={
+                "shopee_shop": cls.SHOP_ID,
+            },
+            pluck="name",
+        )
+        for product_id in product_ids:
+            frappe.get_doc("Shopee Product", product_id).delete()
+
         cls.shop.delete()
         cls.token.delete()
 
     def tearDown(self):
         frappe.set_user("Administrator")
+
+        order_ids = frappe.db.get_list(
+            "Shopee Order",
+            filters={
+                "shopee_shop": self.SHOP_ID,
+            },
+            pluck="name",
+        )
+        for order_id in order_ids:
+            frappe.get_doc("Shopee Order", order_id).delete()
+
         product_ids = frappe.db.get_list(
             "Shopee Product",
             filters={
@@ -139,4 +160,22 @@ class ShopeeProductTest(TestCase):
         )
 
     def test_update_or_insert_with_items(self):
-        self.order.update_or_insert_with_items()
+        with mock.patch(
+            "shopee_open_api.shopee_models.base.ShopeeResponseBaseClass.client"
+        ) as mock_client, mock.patch(
+            "shopee_open_api.shopee_models.product.Product.client",
+        ) as mock_product_client:
+            mock_product_client.product.get_model_list.return_value = (
+                get_model_list_response
+            )
+            get_item_base_info_response["response"]["item_list"][0][
+                "item_id"
+            ] = self.fake_item_id
+
+            mock_client.product.get_item_base_info.return_value = (
+                get_item_base_info_response
+            )
+
+            # for order_item in self.order.order_items:
+            #     print(order_item.get_product_instance().item_id)
+            self.order.update_or_insert_with_items()
