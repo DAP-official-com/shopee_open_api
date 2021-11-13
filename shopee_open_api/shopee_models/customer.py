e46cf63e51f02092abf2baddc5d2302e4768d808
Siwat Tansiri <tansirijames@gmail.com>
2021-11-13 14:09:41 +0700

    Add customer class to handle shopee order customer

---
+++ shopee_open_api/shopee_models/customer.py
@@ -0,0 +1,76 @@
+from .base import ShopeeResponseBaseClass
+from .payment_escrow import PaymentEscrow
+from .order_item import OrderItem
+from .product import Product
+import frappe
+from shopee_open_api.utils.datetime import datetime_string_from_unix
+
+
+class Customer(ShopeeResponseBaseClass):
+    """A class representing a shopee customer"""
+
+    DOCTYPE = "Customer"
+
+    DATA_FIELDS = (
+        "customer_type",
+        "customer_group",
+        "address_type",
+        "customer_name",
+        "territory",
+        "username",
+        "user_id",
+        "shopee_user_id",
+        "mobile_no",
+    )
+
+    @classmethod
+    def from_shopee_customer(cls, *args, **kwargs):
+        return cls(args, kwargs)
+
+    def update_or_insert(self) -> None:
+        if self.is_existing_in_database:
+            customer = frappe.get_doc(self.DOCTYPE, self.get_primary_key())
+        else:
+            customer = frappe.get_doc(
+                {
+                    "doctype": self.DOCTYPE,
+                }
+            )
+
+        for field in self.DATA_FIELDS:
+            setattr(customer, field, getattr(self, field))
+
+        customer.save()
+
+    @property
+    def is_existing_in_database(self) -> bool:
+        return bool(
+            frappe.db.exists(
+                {
+                    "doctype": self.DOCTYPE,
+                    "shopee_user_id": self.shopee_user_id,
+                    "customer_name": self.customer_name,
+                }
+            )
+        )
+
+    def get_primary_key(self) -> str:
+        """Get name (primary key) field from existing customer, otherwise throw DoesNotExistError"""
+
+        try:
+            return frappe.db.get_list(
+                self.DOCTYPE,
+                filters={
+                    "shopee_user_id": self.shopee_user_id,
+                    "customer_name": self.customer_name,
+                },
+                pluck="name",
+            )[0]
+        except IndexError as e:
+            raise frappe.exceptions.DoesNotExistError(
+                f"Customer with Shopee user id {self.shopee_user_id} does not exist in the database"
+            )
+
+    @property
+    def customer_detail(self) -> dict:
+        return {attribute: getattr(self, attribute) for attribute in self.DATA_FIELDS}
--- shopee_open_api/shopee_open_api/doctype/shopee_order/shopee_order.py
+++ shopee_open_api/shopee_open_api/doctype/shopee_order/shopee_order.py
@@ -1,8 +1,29 @@
 # Copyright (c) 2021, Dap Official and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from shopee_open_api.shopee_models.customer import Customer
+
 
 class ShopeeOrder(Document):
-	pass
+    @property
+    def customer_detail(self):
+
+        customer = {}
+
+        # data for Customer Doctype
+        customer["customer_type"] = "Individual"
+        customer["customer_group"] = "Individual"
+        customer["address_type"] = "Postal"
+        customer["customer_name"] = self.recipient_name
+        customer["territory"] = frappe.db.get_default("Country")
+        customer["username"] = self.buyer_username
+        customer["user_id"] = self.buyer_user_id
+        customer["shopee_user_id"] = self.buyer_user_id
+        customer["mobile_no"] = self.recipient_phone
+
+        return customer
+
+    def get_customer_instance(self):
+        return Customer.from_shopee_customer(**self.customer_detail)
