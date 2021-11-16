578314702c5ff311493e64f7ffd709c43101dad9
Siwat Tansiri <tansirijames@gmail.com>
2021-11-16 07:24:06 +0700

    Update territory default value

--- shopee_open_api/shopee_models/tests/mock_data/customer.py
+++ shopee_open_api/shopee_models/tests/mock_data/customer.py
@@ -3,8 +3,8 @@ import frappe
 customer_data = {
     "customer_type": "Individual",
     "customer_group": "Individual",
-    "customer_name": "Firstname Lastname",
-    "territory": "Thailand",
+    "customer_name": "Fakefirstname Fakelastname",
+    "territory": frappe.db.get_default("Country"),
     "username": "shopee_username",
     "user_id": "shopee_user_id",
     "shopee_user_id": "shopee_user_id",
