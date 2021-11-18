14055d7edecf7ce9d1816ddc448aee3372dde3ff
kongtri <kongtri.ac@gmail.com>
2021-11-18 16:00:55 +0700

    fix docker file for gitlab

--- docker/nginx/install_app.sh
+++ docker/nginx/install_app.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
+# Define default argument for erpnext application installation
+set -e
+
 APP_NAME=${1}
+APP_REPO=${2}
+APP_BRANCH=${3}
+FRAPPE_BRANCH=${4}
 
-echo -e "frappe\n${APP_NAME}" > /home/frappe/frappe-bench/sites/apps.txt
-cd /home/frappe/frappe-bench/apps/${APP_NAME} && yarn
+[ "${APP_BRANCH}" ] && BRANCH="-b ${APP_BRANCH}"
 
-cd /home/frappe/frappe-bench/apps/frappe
-yarn production --skip_frappe --app ${APP_NAME}
+mkdir -p /home/frappe/frappe-bench
+cd /home/frappe/frappe-bench
+mkdir -p apps "sites/assets/${APP_NAME}"
+echo -ne "frappe\n${APP_NAME}" >sites/apps.txt
 
-mkdir -p /home/frappe/frappe-bench/sites/assets/${APP_NAME}
-cp -R /home/frappe/frappe-bench/apps/${APP_NAME}/${APP_NAME}/public/* /home/frappe/frappe-bench/sites/assets/${APP_NAME} 2>/dev/null || :
-cp -R /home/frappe/frappe-bench/apps/${APP_NAME}/node_modules /home/frappe/frappe-bench/sites/assets/${APP_NAME}/ 2>/dev/null || :
+# make Erpnext app directory in apps directory and get-app from git repo
+git clone --depth 1 -b "${FRAPPE_BRANCH}" https://github.com/frappe/frappe apps/frappe
+# shellcheck disable=SC2086
+git clone --depth 1 -b ${BRANCH} ${APP_REPO} apps/${APP_NAME}
 
-echo "rsync -a --delete /var/www/html/assets/${APP_NAME} /assets" >> /rsync
+# install node dependency 
+echo "Install frappe NodeJS dependencies . . ."
+cd apps/frappe
+yarn --pure-lockfile
+
+echo "Install ${APP_NAME} NodeJS dependencies . . ."
+yarn --pure-lockfile --cwd "../${APP_NAME}"
+
+echo "Build ${APP_NAME} assets . . ."
+yarn production --app "${APP_NAME}"
+
+cd /home/frappe/frappe-bench
+# shellcheck disable=SC2086
+cp -R apps/${APP_NAME}/${APP_NAME}/public/* sites/assets/${APP_NAME}
+
+# Add frappe and all the apps available under in frappe-bench here
+echo "rsync -a --delete /var/www/html/assets/frappe /assets" >/rsync
+echo "rsync -a --delete /var/www/html/assets/${APP_NAME} /assets" >>/rsync
 chmod +x /rsync
+
+rm sites/apps.txt
