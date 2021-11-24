c81025421f65e73c938b6c3cbc0883b96fc48e55
Siwat Tansiri <tansirijames@gmail.com>
2021-11-24 15:58:43 +0700

    Update business insights report

--- shopee_open_api/shopee_open_api/report/shopee_business_insights/shopee_business_insights.py
+++ shopee_open_api/shopee_open_api/report/shopee_business_insights/shopee_business_insights.py
@@ -19,10 +19,11 @@ def execute(filters=None):
         if default_filters["date_end"] < default_filters["date_start"]:
             frappe.throw("The end date cannot be earlier than the start date")
 
+    # chart section
     sales_data = frappe.db.sql(
         """
         SELECT
-            DATE(shopee_order.create_time) AS date_order,
+            CONCAT(YEAR(shopee_order.create_time), '-', MONTH(shopee_order.create_time)) AS month_order,
             COUNT(shopee_order.name) AS number_of_orders,
             SUM(shopee_order.total_amount) AS total_revenue
         FROM
@@ -30,17 +31,34 @@ def execute(filters=None):
         WHERE
             (%(date_start)s IS NULL OR DATE(shopee_order.create_time) >= %(date_start)s) AND
             (%(date_end)s IS NULL OR DATE(shopee_order.create_time) <= %(date_end)s) AND
-            (%(order_status)s IS NULL OR shopee_order.order_status = %(order_status)s) AND
-            shopee_order.order_status != 'CANCELLED'
+
+            -- select order status that match or select all except cancelled
+            ((shopee_order.order_status = %(order_status)s) OR
+            (%(order_status)s IS NULL AND shopee_order.order_status != 'CANCELLED'))
         GROUP BY
-            DATE(shopee_order.create_time)
+            month_order
+        ORDER BY
+            DATE(shopee_order.create_time) ASC
         """,
         values=default_filters,
     )
 
+    chart = {
+        "data": {
+            "labels": [row[0] for row in sales_data],
+            "datasets": [
+                {"name": "Revenue", "values": [row[2] for row in sales_data]},
+            ],
+        },
+        "type": "line",
+        "title": "Total Monthly Revenue",
+    }
+
+    ## report table section
     top_products_data = frappe.db.sql(
         """
         SELECT 
+            CONCAT('<img src="', product.image, '" width="100"/>') AS image, 
             product.item_name AS item_name,
             SUM(order_item.qty) AS qty,
             SUM(order_item.qty * order_item.model_discounted_price) AS total_revenue
@@ -51,9 +69,12 @@ def execute(filters=None):
         WHERE
             (%(date_start)s IS NULL OR DATE(shopee_order.create_time) >= %(date_start)s) AND
             (%(date_end)s IS NULL OR DATE(shopee_order.create_time) <= %(date_end)s) AND
-            (%(order_status)s IS NULL OR shopee_order.order_status = %(order_status)s) AND
-            shopee_order.order_status != 'CANCELLED'
+
+            -- select order status that match or select all except cancelled
+            ((shopee_order.order_status = %(order_status)s) OR -- 
+            (%(order_status)s IS NULL AND shopee_order.order_status != 'CANCELLED'))
         GROUP BY
+            product.image,
             product.item_name
         ORDER BY
             total_revenue DESC
@@ -62,6 +83,7 @@ def execute(filters=None):
     )
 
     columns = [
+        {"fieldname": "product_image", "label": "Image", "fieldtype": "Attach Image"},
         {"fieldname": "item_name", "label": "Item Name", "fieldtype": "Data"},
         {"fieldname": "qty", "label": "QTY", "fieldtype": "Int"},
         {"fieldname": "total_venue", "label": "Total Revenue", "fieldtype": "Currency"},
@@ -69,6 +91,7 @@ def execute(filters=None):
 
     data = top_products_data
 
+    ## report summary section
     total_revenue = sum([row[2] for row in sales_data])
     number_of_orders = sum([row[1] for row in sales_data])
 
@@ -78,8 +101,10 @@ def execute(filters=None):
         WHERE
             (%(date_start)s IS NULL OR DATE(create_time) >= %(date_start)s) AND
             (%(date_end)s IS NULL OR DATE(create_time) <= %(date_end)s) AND
-            (%(order_status)s IS NULL OR order_status = %(order_status)s) AND
-            order_status != 'CANCELLED'
+
+            -- select order status that match or select all except cancelled
+            ((order_status = %(order_status)s) OR
+            (%(order_status)s IS NULL AND order_status != 'CANCELLED'))
         """,
         values=default_filters,
         as_dict=True,
@@ -93,13 +118,37 @@ def execute(filters=None):
     report_summary = [
         {
             "label": "Total Revenue",
-            "value": f"{frappe.db.get_default('Currency')} {total_revenue:,}",
+            "value": f"{frappe.db.get_default('Currency')} {total_revenue:,}"
+            if total_revenue
+            else 0,
         },
         {"label": "No. of Orders", "value": f"{number_of_orders:,}"},
         {
             "label": "Avg. Order Value",
-            "value": f"{frappe.db.get_default('Currency')} {average_order_value:,.2f}",
+            "value": f"{frappe.db.get_default('Currency')} {average_order_value:,.2f}"
+            if total_revenue
+            else 0,
         },
     ]
 
-    return columns, data, None, None, report_summary
+    message = "Sales summary "
+
+    message += (
+        "from %(date_start)s " % default_filters
+        if default_filters.get("date_start")
+        else ""
+    )
+
+    message += (
+        f"until {default_filters.get('date_end')} "
+        if default_filters.get("date_end")
+        else ""
+    )
+
+    message += (
+        f"(order status: {default_filters.get('order_status')})"
+        if default_filters.get("order_status")
+        else ""
+    )
+
+    return columns, data, message, chart, report_summary
