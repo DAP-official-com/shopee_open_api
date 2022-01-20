e53bc2e92f6dc2a5b1d6798dd2950efe000b42d2
Siwat Tansiri <tansirijames@gmail.com>
2022-01-20 13:15:41 +0700

    feat: Set ERPNext item defaults (company, warehouse, default price list). Closes #14

--- shopee_open_api/shopee_open_api/doctype/shopee_product/shopee_product.py
+++ shopee_open_api/shopee_open_api/doctype/shopee_product/shopee_product.py
@@ -3,6 +3,8 @@
 
 import frappe
 from frappe.model.document import Document
+from erpnext.stock.doctype.item.item import Item
+from shopee_open_api.shopee_open_api.doctype.shopee_shop import shopee_shop
 
 
 class ShopeeProduct(Document):
@@ -21,7 +23,7 @@ class ShopeeProduct(Document):
         item = self.create_new_item_from_product()
         self.add_item_to_product(item=item)
 
-    def create_new_item_from_product(self):
+    def create_new_item_from_product(self) -> Item:
         """Create a new ERPNext item based on current product."""
 
         item = frappe.new_doc("Item")
@@ -31,9 +33,27 @@ class ShopeeProduct(Document):
         item.standard_rate = self.current_price
         item.item_group = "Products"
 
+        self.create_item_defaults(item)
+
         item.save()
 
         return item
+
+    def create_item_defaults(self, item: Item) -> None:
+        defaults = frappe.defaults.get_defaults() or {}
+
+        item.append(
+            "item_defaults",
+            {
+                "company": defaults.get("company"),
+                "default_warehouse": self.get_shopee_shop_document()
+                .get_warehouse()
+                .name,
+                "default_price_list": self.get_shopee_shop_document()
+                .get_price_list()
+                .name,
+            },
+        )
 
     def get_item(self):
         """Get ERPNext Item instance."""
@@ -53,3 +73,7 @@ class ShopeeProduct(Document):
 
         self.item = item.name
         self.save()
+
+    def get_shopee_shop_document(self) -> shopee_shop.ShopeeShop:
+        """Get an instance of item's shop"""
+        return frappe.get_doc("Shopee Shop", self.shopee_shop)
