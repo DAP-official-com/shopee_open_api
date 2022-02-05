3b857b74a2496ad9be7fc594e1e22417ebc684b1
Siwat Tansiri <tansirijames@gmail.com>
2022-02-05 21:12:56 +0700

    feat: Add Shopee Order Queue doctype

---
+++ shopee_open_api/shopee_open_api/doctype/shopee_order_queue/shopee_order_queue.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Dap Official and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Shopee Order Queue', {
+	// refresh: function(frm) {
+
+	// }
+});
