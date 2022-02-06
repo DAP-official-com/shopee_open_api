15a4bb088ace5b59608ef427531095b2c0bb5139
Siwat Tansiri <tansirijames@gmail.com>
2022-02-06 09:03:17 +0700

    feat: Add processing order task

--- shopee_open_api/scheduled_tasks/tasks.py
+++ shopee_open_api/scheduled_tasks/tasks.py
@@ -11,3 +11,18 @@ def update_products():
 
 def start_pulling_products(**kwargs):
     enqueue("shopee_open_api.scheduled_tasks.product_tasks.pull_products", **kwargs)
+
+
+def process_order_queue():
+
+    queue_items = [
+        frappe.get_doc("Shopee Order Queue", name)
+        for name in frappe.get_all("Shopee Order Queue", pluck="name")
+    ]
+
+    for queue_item in queue_items:
+
+        if queue_item.get_shop_document().hold_order:
+            continue
+
+        queue_item.process_order()
