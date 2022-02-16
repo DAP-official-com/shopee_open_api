6883812112d90687fe3dd43b0452d9770082fc01
Siwat Tansiri <tansirijames@gmail.com>
2022-02-16 15:52:19 +0700

    feat: Add order discounts

--- shopee_open_api/shopee_open_api/doctype/shopee_order/shopee_order.py
+++ shopee_open_api/shopee_open_api/doctype/shopee_order/shopee_order.py
@@ -148,12 +148,21 @@ class ShopeeOrder(Document):
         new_sales_order.set_warehouse = shop.get_warehouse().name
         new_sales_order.selling_price_list = shop.get_price_list().name
         new_sales_order.sales_partner = "Shopee"
+        
+        # Transaction fee
         new_sales_order.commission_rate = (
             self.seller_transaction_fee / self.original_price
         ) * 100
         new_sales_order.shipping_rule = self.get_or_create_shipping_rule().name
+
+        # Discounts
+        new_sales_order.discount_amount = (
+            self.seller_discount + self.voucher_from_seller
+        )
+
         new_sales_order.shopee_order = self.name
 
+        # Shipping fee
         shipping_charge = new_sales_order.append("taxes", {})
         shipping_charge.charge_type = "Actual"
         shipping_charge.account_head = (
@@ -163,6 +172,7 @@ class ShopeeOrder(Document):
         shipping_charge.rate = self.buyer_paid_shipping_fee
         shipping_charge.tax_amount = self.buyer_paid_shipping_fee
 
+        # Purchased items
         for order_item in self.order_items:
             shopee_product = order_item.get_shopee_product()
             item = shopee_product.get_item()
