18a1485f944eaf66cd2cb0acddb9a72f42d9ed9a
Siwat Tansiri <tansirijames@gmail.com>
2022-02-20 06:47:01 +0700

    feat: Handle unix as local time

--- shopee_open_api/utils/datetime.py
+++ shopee_open_api/utils/datetime.py
@@ -1,12 +1,20 @@
-from datetime import datetime
+import frappe
+import pytz
+
+from datetime import datetime, timezone
 
 
 def datetime_string_from_unix(unix: float) -> str:
-    return datetime_to_str(datetime_from_unix(unix))
+    datetime_string = datetime_to_str(datetime_from_unix(unix))
+    return datetime_string
 
 
 def datetime_from_unix(unix: float) -> datetime:
-    return datetime.utcfromtimestamp(unix)
+    timezone_name = frappe.db.get_default("time_zone")
+    timezone_obj = pytz.timezone(timezone_name)
+    utc_datetime = datetime.utcfromtimestamp(unix)
+    local_datetime = utc_datetime.replace(tzinfo=timezone.utc).astimezone(timezone_obj)
+    return local_datetime
 
 
 def datetime_to_str(datetime: datetime) -> str:
