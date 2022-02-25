437d5b6a47c271319d442fcee8bbe78d26de7e93
Siwat Tansiri <tansirijames@gmail.com>
2022-02-25 12:57:09 +0700

    feat: Add method to process payments from Shopee

--- shopee_open_api/shopee_open_api/doctype/shopee_wallet_transaction/shopee_wallet_transaction.py
+++ shopee_open_api/shopee_open_api/doctype/shopee_wallet_transaction/shopee_wallet_transaction.py
@@ -3,14 +3,22 @@
 
 import enum
 import frappe
+import inspect
 from frappe.model.document import Document
 from typing import List
+
+from shopee_open_api.exceptions import PaymentProcessingError
+from shopee_open_api.utils.datetime import datetime_from_unix
+from shopee_open_api.shopee_models.order import Order
 
 
 class TransactionType(enum.Enum):
     ALL = "WITHDRAWAL%"
     REQUESTED_WITHDRAWAL = "WITHDRAWAL_CREATED"
     COMPLETED_WITHDRAWAL = "WITHDRAWAL_COMPLETED"
+    ESCROW_VERIFIED_ADD = "ESCROW_VERIFIED_ADD"
+    ADJUSTMENT_MINUS = "ADJUSTMENT_MINUS"
+    ADJUSTMENT_ADD = "ADJUSTMENT_ADD"
 
 
 class ShopeeWalletTransaction(Document):
@@ -199,6 +207,154 @@ class ShopeeWalletTransaction(Document):
             return frappe.get_doc("Shopee Wallet Transaction", next_transaction[0])
         return None
 
+    def process_payment(self) -> None:
+        """
+        Process transactions under current withdrawal request.
+
+        Each time there is a withdrawal request, there are multiple transactions that contribute to the total amount.
+        A withdrawal request transaction is accompanied by a withdrawl complete transaction, usually a day after.
+        That's why we check that the withdrawal complete transaction is present before processing the request.
+
+        For transaction type "ESCROW_VERIFIED_ADD", the amount will be the final amount paid by Shopee to seller,
+        excluding the transaction fee. Since the transaction fee was recorded in sales invoice as commission, it does
+        not reflect on the actual account yet, and that the receivable amount will be higher than the escrow amount.
+        Our approach is to collect the full payment, then record transaction fee as an expense.
+        However, some orders can be paid partially twice, we cannot collect the transaction fee both times. Hence, the transaction fee
+        paid condition was checked.
+
+        There are also some orders that do not exist in the database, or that they do not have sales invoice yet
+        due to myriad reasons. We record those transactions as indirect income and ignore the transaction fee altogether.
+
+        For transaction types "ADJUSTMENT_ADD" and "ADJUSTMENT_MINUS", they are recorded as indirect income,
+        either on credit or debit depending on the positivity or negativity of the amount.
+
+        Once all the transactions are recorded, we need to record payment for the transaction fee amount paid by end-customer
+        to Shopee, so that the Debit must equal Credit.
+        """
+
+        if self.payment_processed:
+            return
+
+        if self.transaction_type != "WITHDRAWAL_CREATED":
+            raise PaymentProcessingError(
+                f"Method {inspect.stack()[0][3]} can only be called with transaction type 'WITHDRAWAL_CREATED'"
+            )
+
+        withdrawal_complete_transaction = self.get_withdrawal_complete()
+        if withdrawal_complete_transaction is None:
+            return
+
+        shopee_shop = self.get_shopee_shop_document()
+        shops_cash_account = shopee_shop.get_or_create_cash_account()
+        shops_receivable_account = shopee_shop.get_receivable_account()
+        shops_indirect_income_account = (
+            shopee_shop.get_or_create_indirect_income_account()
+        )
+
+        payment_datetime = datetime_from_unix(
+            withdrawal_complete_transaction.create_time
+        )
+
+        total_balance = {"debit": 0, "credit": 0}
+
+        transactions_to_process = self.get_orders_for_withdrawal()
+
+        journal_entry = frappe.new_doc("Journal Entry")
+        journal_entry.posting_date = payment_datetime.date()
+        journal_entry.user_remark = f"""Withdrawal Request ID: {self.name}\nWithdrawal Complete ID: {withdrawal_complete_transaction.name}"""
+
+        journal_transaction = journal_entry.append("accounts", {})
+        journal_transaction.account = shops_cash_account.name
+        journal_transaction.debit_in_account_currency = abs(self.amount)
+        journal_transaction.user_remark = f"""Transaction Type: {self.transaction_type}\nTransaction id: {self.name}\nOrder SN: {self.order_sn}\nTransaction reason: {self.reason}"""
+
+        total_balance["debit"] = +abs(self.amount)
+
+        for transaction in transactions_to_process:
+            journal_transaction = journal_entry.append("accounts", {})
+
+            if (
+                transaction.get("transaction").transaction_type
+                == TransactionType.ESCROW_VERIFIED_ADD.value
+            ):
+                order_sn = transaction.get("transaction").order_sn
+
+                if (
+                    frappe.db.exists("Shopee Order", order_sn)
+                    and frappe.get_doc("Shopee Order", order_sn).sales_invoice
+                ):
+                    shopee_order = frappe.get_doc("Shopee Order", order_sn)
+                    journal_transaction.account = shops_receivable_account.name
+                    journal_transaction.party_type = "Customer"
+                    journal_transaction.party = shopee_order.customer
+
+                    if shopee_order.transaction_fee_paid:
+                        amount_to_credit = transaction.get("amount")
+                    else:
+                        amount_to_credit = (
+                            transaction.get("amount")
+                            + shopee_order.seller_transaction_fee
+                        )
+                        shopee_order.transaction_fee_paid = True
+                        shopee_order.save()
+
+                    journal_transaction.credit_in_account_currency = amount_to_credit
+                else:
+                    journal_transaction.account = shops_indirect_income_account.name
+                    amount_to_credit = transaction.get("amount")
+                    journal_transaction.credit_in_account_currency = amount_to_credit
+                    journal_transaction.party_type = "Supplier"
+                    journal_transaction.party = "Shopee"
+
+                total_balance["credit"] += amount_to_credit
+
+            if (
+                transaction.get("transaction").transaction_type
+                == TransactionType.ADJUSTMENT_ADD.value
+            ):
+                journal_transaction.account = shops_indirect_income_account.name
+                amount_to_credit = transaction.get("amount")
+                journal_transaction.credit_in_account_currency = amount_to_credit
+                total_balance["credit"] += amount_to_credit
+
+            if (
+                transaction.get("transaction").transaction_type
+                == TransactionType.ADJUSTMENT_MINUS.value
+            ):
+                journal_transaction.account = shops_indirect_income_account.name
+                amount_to_credit = transaction.get("amount")
+                journal_transaction.credit_in_account_currency = amount_to_credit
+                total_balance["credit"] += amount_to_credit
+
+            journal_transaction.user_remark = f"""Transaction Type: {transaction.get("transaction").transaction_type}\nTransaction id: {transaction.get("transaction").name}\nOrder SN: {transaction.get("transaction").order_sn}\nTransaction reason: {transaction.get("transaction").reason}"""
+
+            print(total_balance)
+
+        """Pay remaining amount as commission (transaction fee)"""
+        balance_difference = total_balance.get("credit") - total_balance.get("debit")
+        if balance_difference != 0:
+            journal_transaction = journal_entry.append("accounts", {})
+            journal_transaction.account = shopee_shop.get_sales_expenses_account().name
+            journal_transaction.party_type = "Supplier"
+            journal_transaction.party = "Shopee"
+            journal_transaction.debit_in_account_currency = balance_difference
+            journal_transaction.user_remark = "Commission (Transaction fee)"
+
+            total_balance["debit"] += balance_difference
+
+        print(total_balance)
+
+        withdrawal_complete_transaction.payment_processed = True
+        withdrawal_complete_transaction.payment_processed_datetime = frappe.utils.now()
+
+        self.payment_processed = True
+        self.payment_processed_datetime = frappe.utils.now()
+        self.save()
+
+        journal_entry.insert()
+        journal_entry.submit()
+        journal_entry.save()
+
     def get_shopee_order_document(self):
         """Get an instance of Shopee Order from attribute order_sn."""
 
@@ -207,3 +363,8 @@ class ShopeeWalletTransaction(Document):
         if not frappe.db.exists("Shopee Order", self.order_sn):
             return None
         return frappe.get_doc("Shopee Order", self.order_sn)
+
+    def get_shopee_shop_document(self):
+        """Get Shopee Shop document for current transaction."""
+
+        return frappe.get_doc("Shopee Shop", self.shopee_shop)
